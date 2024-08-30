debbe9366cd43fb5e5039cc35748e9e53a49432b
anjaliy11 <anjaliyadavknp9450@gmail.com>
2024-08-31 00:35:18 +0530

    updated file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./src/config/db');
 
 
 const app = express();
@@ -14,12 +14,12 @@ connectDB();
 app.use(express.json());
 
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/models_project', require('./routes/projectRoutes')); 
-app.use('/api/seminar', require('./routes/seminarRoutes')); 
-app.use('/api/chat',require('./routes/chatRoutes'));
+app.use('/api/auth', require('./src/routes/auth'));
+app.use('/api/models_project', require('./src/routes/projectRoutes')); 
+app.use('/api/seminar', require('./src/routes/seminarRoutes')); 
+app.use('/api/chat',require('./src/routes/chatRoutes'));
 ;
-app.use('/api/forum',  require('./routes/forumRoutes'));
+app.use('/api/forum',  require('./src/routes/forumRoutes'));
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
